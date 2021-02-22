fdf2efd45405e1bef91d9193faeccd11f8a55ccc
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-02-22 23:54:03 +0100

    added test stuff + started score calculation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,5 +4,12 @@ class GamesController < ApplicationController
   end
 
   def score
+    # binding.pry
+
+
+    # The word cant be built out of the original grid
+    # The word is valid according to the grid, but is not a valid English word
+    # The word is valid according to the grid and is an English word
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # get 'games/score'
   get 'new', to: 'games#new'
   get 'score', to: 'games#score'
+  post 'score', to: 'games#score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
